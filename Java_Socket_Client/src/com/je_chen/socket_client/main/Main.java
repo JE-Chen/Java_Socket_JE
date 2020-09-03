2008969e922d32bcd2ec9a49756bcc1999642cdf
JE-Chen(陳冠穎) <33644111+JE-Chen@users.noreply.github.com>
2020-09-04 00:12:55 +0800

    Add files via upload

--- Java_Socket_Client/src/com/je_chen/socket_client/main/Main.java
+++ Java_Socket_Client/src/com/je_chen/socket_client/main/Main.java
@@ -8,13 +8,8 @@ public class Main {
 
     public static void main(String[] args){
 
-        OneConnect_Server_SocketClient Client1 = new OneConnect_Server_SocketClient("Socket1","127.0.0.1",5555);
-        OneConnect_Server_SocketClient Client2 = new OneConnect_Server_SocketClient("Socket2","127.0.0.1",5555);
-        Client1.Set_Test_Time(1500);
-        Client2.Set_Test_Time(3000);
-        Thread Test1 = new Thread(Client1);
-        Thread Test2 = new Thread(Client2);
-        Test1.start();
-        Test2.start();
+        for(int i=0;i<=100;i++){
+            new Thread(new OneConnect_Server_SocketClient("Socket"+Integer.toString(i),"127.0.0.1",5555)).start();
+        }
     }
 }
