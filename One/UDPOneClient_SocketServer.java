051a34d387b56f8dd666736c7b2e9e4ef1aa0021
JE-Chen(陳冠穎) <33644111+JE-Chen@users.noreply.github.com>
2020-09-18 01:51:20 +0800

    Add files via upload

--- One/UDPOneClient_SocketServer.java
+++ One/UDPOneClient_SocketServer.java
@@ -3,29 +3,33 @@ package com.je_chen.socket_server.One;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.InetAddress;
 import java.net.SocketException;
 
 public class UDPOneClient_SocketServer implements Runnable{
 
     private boolean Running = true;
     private byte[] buffer = new byte[20];
+    private int port;
     private DatagramSocket datagramSocket;
 
     public void setDatagramSocket(int port) throws SocketException {
         datagramSocket = new DatagramSocket(port);
     }
 
+    public void setPort(int port){
+        this.port=port;
+    }
+
     @Override
     public void run() {
         while (Running) {
             try {
+            //receive data
             DatagramPacket datagramPacket = new DatagramPacket(buffer, buffer.length);
             datagramSocket.receive(datagramPacket);
-            StringBuilder messageBuilder = new StringBuilder();
-            for (int message = 0; message < buffer.length; message++) {
-                messageBuilder.append(Character.toString(buffer[message]));
-            }
-            System.out.println(messageBuilder);
+            String message = new String(datagramPacket.getData(),0,datagramPacket.getLength());
+            System.out.println(message);
             } catch (IOException e) {
                 e.printStackTrace();
             }
