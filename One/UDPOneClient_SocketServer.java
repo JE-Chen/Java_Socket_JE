6aa775945095ea8589aee6b2d7dc349c9b4fe739
JE-Chen(陳冠穎) <33644111+JE-Chen@users.noreply.github.com>
2020-09-18 00:41:28 +0800

    Add files via upload

--- One/UDPOneClient_SocketServer.java
+++ One/UDPOneClient_SocketServer.java
@@ -3,18 +3,32 @@ package com.je_chen.socket_server.One;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.SocketException;
 
-public class UDPOneClient_SocketServer {
+public class UDPOneClient_SocketServer implements Runnable{
 
-    public void run() throws IOException {
-        byte[] buffer = new byte[20];
-        DatagramPacket datagramPacket = new DatagramPacket(buffer, 20);
-        DatagramSocket datagramSocket = new DatagramSocket(5555);
-        datagramSocket.receive(datagramPacket);
-        StringBuilder messageBuilder = new StringBuilder();
-        for(int message =0 ; message < buffer.length ; message++){
-            messageBuilder.append(Character.toString(buffer[message]));
+    private boolean Running = true;
+    private byte[] buffer = new byte[20];
+    private DatagramSocket datagramSocket;
+
+    public void setDatagramSocket(int port) throws SocketException {
+        datagramSocket = new DatagramSocket(port);
+    }
+
+    @Override
+    public void run() {
+        while (Running) {
+            try {
+            DatagramPacket datagramPacket = new DatagramPacket(buffer, buffer.length);
+            datagramSocket.receive(datagramPacket);
+            StringBuilder messageBuilder = new StringBuilder();
+            for (int message = 0; message < buffer.length; message++) {
+                messageBuilder.append(Character.toString(buffer[message]));
+            }
+            System.out.println(messageBuilder);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
-        System.out.println(messageBuilder);
     }
 }
--- Test/UDPTest.java
+++ Test/UDPTest.java
@@ -3,15 +3,20 @@ package com.je_chen.socket_server.Test;
 import com.je_chen.socket_server.One.UDPOneClient_SocketServer;
 
 import java.io.IOException;
+import java.net.SocketException;
 
 public class UDPTest {
 
     public static void main(String[] argv){
+
+        UDPOneClient_SocketServer udpOneClient_socketServer = new UDPOneClient_SocketServer();
+
         try {
-            new UDPOneClient_SocketServer().run();
-        } catch (IOException e) {
+            udpOneClient_socketServer.setDatagramSocket(5555);
+        } catch (SocketException e) {
             e.printStackTrace();
         }
-    }
 
+        new Thread(udpOneClient_socketServer).start();
+    }
 }
