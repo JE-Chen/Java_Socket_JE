2909422d362a0169e53d05b2e07f1aabead2c73f
JE-Chen(陳冠穎) <33644111+JE-Chen@users.noreply.github.com>
2020-09-18 17:36:04 +0800

    Add files via upload

--- Test/UDPTest.java
+++ Test/UDPTest.java
@@ -1,22 +1,17 @@
 package com.je_chen.socket_server.Test;
 
-import com.je_chen.socket_server.One.UDPOneClient_SocketServer;
+import com.je_chen.socket_server.Multi.UDPMultiClient_SocketServer;
 
-import java.io.IOException;
 import java.net.SocketException;
 
 public class UDPTest {
 
     public static void main(String[] argv){
-
-        UDPOneClient_SocketServer udpOneClient_socketServer = new UDPOneClient_SocketServer();
-
         try {
-            udpOneClient_socketServer.setDatagramSocket(5555);
+            UDPMultiClient_SocketServer udpMultiClient_socketServer = new UDPMultiClient_SocketServer(5555);
+            new Thread(udpMultiClient_socketServer).start();
         } catch (SocketException e) {
             e.printStackTrace();
         }
-
-        new Thread(udpOneClient_socketServer).start();
     }
 }
